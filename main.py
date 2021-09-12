277012af684ceb21d10d62327586b6537ad5e217
xfaraday <theonlyfaraday@gmail.com>
2021-09-12 13:07:52 -0700

    first swarm attempt

---
+++ main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+from djitellopy import TelloSwarm
+
+def call_swarm():
+	print("assembling swarm")
+	#absolute path m9
+	dronelist='/opt/dronenetlist'
+	fo=open(dronelist, "r")
+	if(fo.read(10)==None):
+		print("drone list not found or null")
+		fo.close()
+		exit()
+	else:
+		fo.close()
+		assembly = TelloSwarm.fromFile(dronelist)
+		return assembly
+
+def connect_and_run(swarm):
+	print("performing health check routine")
+	for drone in swarm:
+		print("IP: " + str(drone) + " Battery: " + str(drone.get_battery()))
+	print("Taking off")
+
+
+
+if __name__ == "__main__":
+	connect_and_run(call_swarm())
